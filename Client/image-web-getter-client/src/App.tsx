0989cd58156886fe75ad767c51e68f933a61d6ff
Alex Canales <alexcanales766@gmail.com>
2022-07-02 16:38:52 -0400

    Added sass styling

--- Client/image-web-getter-client/src/App.tsx
+++ Client/image-web-getter-client/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './styles/style.css';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import Home from './pages/Home';
