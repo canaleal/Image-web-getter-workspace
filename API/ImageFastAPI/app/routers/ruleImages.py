dbd22b66c7b0c5c3efc533148a8901a12345f2c2
Alex Canales <alexcanales766@gmail.com>
2022-07-05 03:27:56 -0400

    Create a get by id route

--- API/ImageFastAPI/app/routers/ruleImages.py
+++ API/ImageFastAPI/app/routers/ruleImages.py
@@ -3,8 +3,8 @@ from fastapi_sqlalchemy import  db
 from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
 from app.schema import RuleImage as SchemaRuleImage
 from app.schema import RuleImage
-
-from app.models import RuleImage as RuleImage
+from typing import Optional, List
+from app.models import RuleImage as ModelRuleImage
 
 
 router = APIRouter(
@@ -19,11 +19,28 @@ async def ruleImage(ruleImage: SchemaRuleImage):
     db.session.commit()
     return db_ruleImage
 
-@router.get('/')
+@router.get('/', response_model=List[SchemaRuleImage])
 async def ruleImage():
-    ruleImages = db.session.query(RuleImage).all()
+    ruleImages = db.session.query(ModelRuleImage).all()
     if not ruleImages:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"No ruleImages found")
       
     return ruleImages
 
+@router.get('/{id}', response_model=SchemaRuleImage)
+async def ruleImage(id: int):
+    ruleImage = db.session.query(ModelRuleImage).filter(ModelRuleImage.id == id).first()
+    if not ruleImage:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"No ruleImage with id: {id} found")
+    return ruleImage
+
+@router.put('/{id}', response_model=SchemaRuleImage)
+async def ruleImage(id: int, ruleImage: SchemaRuleImage):
+    ruleImage = db.session.query(ModelRuleImage).filter(ModelRuleImage.id == id).first()
+    if ruleImage == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"No ruleImage with id: {id} found")
+    
+    ruleImage.delete(synchronize_session=False)
+    db.commit()
+    
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
