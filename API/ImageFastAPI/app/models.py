f0bc860cea11299b05a7097ac7026881aec5af0e
Alex Canales <alexcanales766@gmail.com>
2022-07-05 02:18:27 -0400

    Created fast api project

---
+++ API/ImageFastAPI/app/models.py
@@ -0,0 +1,16 @@
+from pickle import TRUE
+from .database import Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import TIMESTAMP, Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.sql.expression import text
+
+    
+class User(Base):
+    __tablename__ = "rule_general_images"
+    
+    id = Column(Integer, primary_key=True, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+    
+    
